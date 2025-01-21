26f189691f61cce7ccfabc1481cd034b3c93f767
MaraMincho <seemaster300@naver.com>
2025-01-21 14:02:45 +0900

    prj: create prj

---
+++ MaraMinchoTodos/src/main/java/org/example/maraminchotodos/MaraMinchoTodosApplication.java
@@ -0,0 +1,13 @@
+package org.example.maraminchotodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaraMinchoTodosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaraMinchoTodosApplication.class, args);
+    }
+
+}
---
+++ MaraMinchoTodos/src/test/java/org/example/maraminchotodos/MaraMinchoTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.maraminchotodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaraMinchoTodosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
